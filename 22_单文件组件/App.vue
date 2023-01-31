eece5e2123ed526c5c85d52cf019395ed519d1d8
liuxiaolei001 <7711242+liuxiaolei001@user.noreply.gitee.com>
2023-01-31 18:01:43 +0800

    22_单文件组件

---
+++ 22_单文件组件/App.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <div class="demo">
+    <school></school>
+  </div>
+</template>
+
+<script>
+import School from "./School.vue";
+export default {
+  name:'App',//最好和文件名一直
+  components:{
+    School
+  }
+
+}
+
+</script>
---
+++ 22_单文件组件/School.vue
@@ -0,0 +1,34 @@
+
+<template>
+  <div class="demo">
+      <h2>学校名称：{{schoolName}}</h2>
+      <h2>学校地址：{{address}}</h2>
+      <button @click="showName">点击我提示学校名称</button>
+  </div>
+</template>
+
+<script>
+export default {
+   name:'School',//最好和文件名一直
+    data(){
+     return{
+       schoolName:'熊熊幼儿园',
+       address:'狗熊岭'
+     }
+
+
+    },
+    methods:{
+      showName(){
+        alert(this.schoolName)
+      }
+    }
+  }
+
+</script>
+
+<style>
+  .demo{
+  background-color: darkorange;
+  }
+</style>
