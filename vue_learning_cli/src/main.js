4eaea8224275ce1a6760bc22630774e22c6afaed
bear <2453576273@qq.com>
2023-02-07 22:47:21 +0800

    一个bug：mac运行之前的脚手架项目报错，还没解决

--- vue_learning_cli/src/main.js
+++ vue_learning_cli/src/main.js
@@ -1,11 +1,15 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App.vue'
-import plugins from  './plugins'
+import App from './App'
+import router from './router'
+
 Vue.config.productionTip = false
 
-// 使用插件,可以传参
-Vue.use(plugins)
+/* eslint-disable no-new */
 new Vue({
-  el:'#App',
-  render: h => h(App),
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
 })
